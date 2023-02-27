ca0a8eb5ca716d470656d011aa22fe872274e67b
komekami <komekami0131@gmail.com>
2023-02-28 08:42:35 +0900

    refactor: backendディレクトリを削除

--- backend/src/interface/hashtag.interface.ts
+++
@@ -1,6 +0,0 @@
-export class Hashtag {
-  id: number;
-  tagName: string;
-  isSelf: boolean;
-}
-
--- backend/src/interface/tags-find-all.interface.ts
+++
@@ -1,8 +0,0 @@
-import { Tweet } from './tweet.interface';
-
-export class TagsFindAll {
-  id: number;
-  tagName: string;
-  isSelf: boolean;
-  tweets: Tweet[];
-}
