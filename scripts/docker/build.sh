ed36856146b2fb5ef39b54edfbab2b61e9d27230
komekami <komekami0131@gmail.com>
2023-02-28 22:59:06 +0900

    chore: Dockerfileでnodeユーザを使用する

---
+++ scripts/docker/build.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+set -eu
+
+# Default param
+IMAGE_NAME="tmp-friends/victo"
+uid=1000
+gid=1000
+user=node
+
+base=$(cd $(dirname $0); pwd)
+docker image build -t $IMAGE_NAME:latest -f $base/Dockerfile . \
+  --build-arg uid=$uid \
+  --build-arg gid=$gid \
+  --build-arg user=$user
