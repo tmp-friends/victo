4353582460a08a177a2454231374210729932755
komekami <komekami0131@gmail.com>
2023-03-05 23:04:19 +0900

    feat: 認証のためFirebaseを導入

---
+++ app/firebase.ts
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBhKZ9qYfac2TOBPPHVltVyE6subi6q0rg",
+  authDomain: "victo-372706.firebaseapp.com",
+  projectId: "victo-372706",
+  storageBucket: "victo-372706.appspot.com",
+  messagingSenderId: "581255006042",
+  appId: "1:581255006042:web:56bbf6b5f96ad4e2ba5563",
+  measurementId: "G-XHL865ELT5"
+}
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig)
