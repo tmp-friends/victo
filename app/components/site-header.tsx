21bd61c7434589ab17ac3f1e67634c3c3de7a144
komekami <komekami0131@gmail.com>
2023-03-11 23:09:39 +0900

    feat: ログインボタンの実装

--- app/components/site-header.tsx
+++ app/components/site-header.tsx
@@ -1,30 +1,48 @@
-import { FC } from "react";
-import Link from "next/link";
-import { Flex, Button } from "@chakra-ui/react";
+import { FC } from "react"
+import Link from "next/link"
+import {
+  Box,
+  Flex,
+  Button,
+  Spacer,
+} from "@chakra-ui/react"
+import { LoginButton } from "./login-button";
+import { useAuthContext } from "../auth/AuthContext";
+import { getCookie } from "typescript-cookie";
 
 export const SiteHeader: FC = () => {
+  const { user } = useAuthContext()
+  const isLogin = !!user
+
+  console.log(isLogin)
   return (
-    <Flex as="header" shadow="md">
-      <Link href="/" passHref>
-        <Button fontSize="md" variant="ghost">
-          Victo
-        </Button>
-      </Link>
-      <Link href="/" passHref>
-        <Button fontSize="sm" variant="ghost">
-          Home
-        </Button>
-      </Link>
-      <Link href="/pickup" passHref>
-        <Button fontSize="sm" variant="ghost">
-          Pickup
-        </Button>
-      </Link>
-      <Link href="/hashtags" passHref>
-        <Button fontSize="sm" variant="ghost">
-          Hashtags
-        </Button>
-      </Link>
-    </Flex>
+    <>
+      <Flex as="header" shadow="md" alignItems="center">
+        <Link href="/" passHref>
+          <Button fontSize="md" variant="ghost">
+            Victo
+          </Button>
+        </Link>
+        <Link href="/" passHref>
+          <Button fontSize="sm" variant="ghost">
+            Home
+          </Button>
+        </Link>
+        <Link href="/pickup" passHref>
+          <Button fontSize="sm" variant="ghost">
+            Pickup
+          </Button>
+        </Link>
+        <Link href="/hashtags" passHref>
+          <Button fontSize="sm" variant="ghost">
+            Hashtags
+          </Button>
+        </Link>
+        <Spacer />
+        <Box m={2}>
+          <LoginButton />
+        </Box>
+      </Flex>
+    </>
   );
 };
