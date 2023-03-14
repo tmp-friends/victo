c1fe403e019324e6e560ad4112f79510e955f524
komekami <komekami0131@gmail.com>
2023-03-14 09:37:16 +0900

    refactor: useContextでProfile型を適用する

--- app/pages/my/index.tsx
+++ app/pages/my/index.tsx
@@ -1,21 +1,13 @@
 import type { NextPage } from "next"
 import { useAuthContext } from "../../auth/AuthContext"
 import { ProfileContent } from "../../components/profile-content";
-import { Profile } from "../../types/profile"
 
 const MyPage: NextPage = () => {
   const user = useAuthContext()
 
-  const profile: Profile = {
-    name: user?.name ?? "",
-    email: user?.email ?? "",
-    image_url: user?.profile_image_url ?? "",
-    is_user: true,
-  }
-
   return (
     <>
-      {ProfileContent(profile)}
+      {ProfileContent(user)}
     </>
   )
 };
