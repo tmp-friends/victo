35c7c047daf56f51d58b1cf0f4cc6483f65ad53b
komekami <komekami0131@gmail.com>
2023-03-28 08:19:13 +0900

    update: SSG対応

--- app/next.config.js
+++ app/next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  async rewrites() {
+    return [
+      {
+        source: `${process.env.NEXT_PUBLIC_API_BASE_PATH}/:match*`,
+        destination: `${process.env.API_BASE_URL}/:match*`,
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
