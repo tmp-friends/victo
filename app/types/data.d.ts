bc000a6ac543b89827dd3e4a4b074e317aa1239c
komekami <komekami0131@gmail.com>
2023-04-04 08:43:38 +0900

    refactor: APIから返却される型定義を1つのファイルにまとめる

---
+++ app/types/data.d.ts
@@ -0,0 +1,42 @@
+// ユーザ
+export type User = {
+  id: number
+  name: string
+  email: string
+  profile_image_url: string
+}
+
+// ツイート
+export type TweetObject = {
+  id: number
+  tweet_id: string
+  text: string
+  retweet_count: number
+  like_count: number
+  author_id: string
+  url: string
+  tweeted_at: Date
+  created_at: Date
+  updated_at: Date
+  hashtag_id: number
+}
+
+// ハッシュタグ
+export type Hashtag = {
+  id: number
+  name: string
+  is_self: boolean
+  created_at: Date
+  updated_at: Date
+  vtuber_id: number
+  vtuber_name: string
+  belongs_to: string
+  profile_image_url: string
+  twitter_user_name: string
+  channel: string
+}
+
+// APIコンテキスト
+export type ApiContext = {
+  apiRootUrl: string
+}
