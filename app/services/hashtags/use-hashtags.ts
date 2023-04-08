cadd3cb9595c37cadec9812782a28594f8ca840d
komekami <komekami0131@gmail.com>
2023-04-08 18:45:52 +0900

    update: 処理速度の観点からEmbedTweetsを独自componentに変更

--- app/services/hashtags/use-hashtags.ts
+++ app/services/hashtags/use-hashtags.ts
@@ -7,10 +7,6 @@ export type UseHashtagsProps = {
    */
   ids?: number[]
   /**
-   * Vtuber情報も取得するか
-   */
-  withVtuber?: boolean
-  /**
    * 取得数
    */
   limit?: number
@@ -22,6 +18,10 @@ export type UseHashtagsProps = {
    * 取得フィールド名
    */
   props?: string[]
+  /**
+   * Vtuber情報も取得するか
+   */
+  withVtuber?: boolean
   /**
    * 初期状態
    */
@@ -50,16 +50,16 @@ export type UseHashtag = {
  */
 const useHashtags = (
   context: ApiContext,
-  { ids, withVtuber, limit, offset, props, initial }: UseHashtagsProps
+  { ids, limit, offset, props, withVtuber, initial }: UseHashtagsProps
 ): UseHashtag => {
   const path = `${context.apiRootUrl}/v1/hashtags`
   const params = new URLSearchParams()
 
   ids && params.append("ids", ids.join(","))
-  withVtuber && params.append("withVtuber", withVtuber.toString())
   limit && params.append("limit", limit.toString())
   offset && params.append("offset", offset.toString())
   props && params.append("props", props.join(","))
+  withVtuber && params.append("withVtuber", withVtuber.toString())
 
   const { data, error } = useSWR<Hashtag[]>(path + "?" + params.toString())
 
