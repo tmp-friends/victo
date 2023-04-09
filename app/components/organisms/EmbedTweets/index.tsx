a48f5d048df74487c9a1e594ab3840d0e5757cbc
komekami <komekami0131@gmail.com>
2023-04-10 07:56:02 +0900

    feat: EmbedTweetsのロジック処理を行うContainerファイルの実装

--- app/components/organisms/EmbedTweets/index.tsx
+++ app/components/organisms/EmbedTweets/index.tsx
@@ -1,65 +1,90 @@
-import { Avatar, Box, Button, Card, CardBody, CardFooter, CardHeader, Flex, Heading, Image, SimpleGrid, Text } from "@chakra-ui/react"
+import { Avatar, Box, Button, Card, CardBody, CardFooter, CardHeader, Flex, Heading, Icon, Img, Link, SimpleGrid, Spacer, Text, textDecoration } from "@chakra-ui/react"
+import { FaTwitter } from "@react-icons/all-files/fa/FaTwitter"
+
 import { Tweet } from "../../../types/data"
 
 interface EmbedTweetsProps {
   /**
-   * Tweetの配列
+   * 取得したページごとのツイート配列
+   * ex. [Array(50), Array(50), Array(50)]
    */
-  tweets?: Tweet[]
+  tweetsChunks?: Tweet[][]
 }
 
-const EmbedTweets: React.FC<EmbedTweetsProps> = ({ tweets }) => {
+const EmbedTweets: React.FC<EmbedTweetsProps> = ({ tweetsChunks }) => {
+  const handleCardClick = (url: string) => {
+    window.open(url, "_blank")
+  }
+
   return (
     <>
-      {!Array.isArray(tweets) || tweets.length === 0 ? (
+      {tweetsChunks === undefined || tweetsChunks.length === 0 ? (
         <Text fontWeight="semibold" align="center">
           Tweets not found
         </Text>
       ) : (
-        <SimpleGrid columns={{ base: 1, md: 2, lg: 3, xl: 4 }} spacing={4}>
-          {tweets.map((v, i) => (
-            <Card key={i} variant="elevated">
-              <CardHeader>
-                <Flex>
-                  <Flex>
+        <>
+          <SimpleGrid columns={{ base: 1, md: 2, lg: 3, xl: 5 }} alignItems="flex-start" spacingX={4} spacingY={8}>
+            {tweetsChunks.map((tweets) => (
+              Array.isArray(tweets) && tweets.map((v, i) => (
+                <Card
+                  key={i}
+                  size="sm"
+                  variant="elevated"
+                  onClick={() => handleCardClick(v.url)}
+                >
+                  {/* <Link href={v.url} isExternal> */}
 
-                    <Avatar name="test" src="https://bit.ly/sage-adebayo" />
-                    <Box>
-                      <Heading size="sm">Author Name</Heading>
-                      <Text>Author UID</Text>
-                    </Box>
+                  <CardHeader>
+                    <Flex alignItems="center">
+                      <Flex>
+                        <Avatar name="test" src="https://bit.ly/sage-adebayo" />
+                        <Box>
+                          <Heading size="sm">{"Author Nameeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee".substring(0, 10)}</Heading>
+                          <Text>Author UID</Text>
+                        </Box>
+                      </Flex>
+                      <Spacer />
+                      <Icon
+                        as={FaTwitter}
+                        boxSize={6}
+                        color="twitter.400"
+                      />
+                    </Flex>
+                  </CardHeader>
 
-                    {/* TODO: TwitterIcon */}
+                  <CardBody>
+                    <Text _hover={{ textDecoration: "none" }}>{v.text}</Text>
+                  </CardBody>
 
-                  </Flex>
-                </Flex>
-              </CardHeader>
-              <CardBody>
-                <Text>{v.text}</Text>
-              </CardBody>
-              <Image
-                objectFit="cover"
-                src={v.media_url[0]}
-                alt={v.text}
-                mx={3}
-                borderRadius="lg"
-              />
+                  <Img
+                    src={v.media_url}
+                    alt={v.text}
+                    mx="auto"
+                    maxH="400px"
+                    borderRadius="lg"
+                    loading="lazy"
+                  />
 
-              <CardFooter
-                justify="space-between"
-                flexWrap="wrap"
-                sx={{
-                  "& > button": {
-                    minW: "136px",
-                  },
-                }}
-              >
-                <Button>Like</Button>
+                  <CardFooter
+                    justify="space-between"
+                    flexWrap="wrap"
+                    sx={{
+                      "& > button": {
+                        minW: "136px",
+                      },
+                    }}
+                  >
+                    <Button>Like</Button>
+                  </CardFooter>
 
-              </CardFooter>
-            </Card>
-          ))}
-        </SimpleGrid>
+                  {/* </Link> */}
+
+                </Card>
+              ))
+            ))}
+          </SimpleGrid>
+        </>
       )
       }
     </>
---
+++ app/containers/EmbedTweetsContainer.tsx
@@ -0,0 +1,76 @@
+import { Text, Box, Button, Spinner } from "@chakra-ui/react"
+
+import { ApiContext, Tweet } from "../types/data"
+import EmbedTweets from "../components/organisms/EmbedTweets"
+import useTweets from "../services/tweets/use-tweets"
+import { useState } from "react"
+
+const context: ApiContext = {
+  apiRootUrl: process.env.NEXT_PUBLIC_API_ROOT_URL ?? "http://localhost:3001",
+}
+
+interface EmbedTweetsContainerProps {
+  /**
+   * ハッシュタグIDの配列
+   */
+  hashtagIds: number[]
+  /**
+   * SSGで取得したツイート配列
+   */
+  initial?: Tweet[][]
+}
+
+const EmbedTweetsContainer = ({ hashtagIds, initial }: EmbedTweetsContainerProps) => {
+  const { tweetsChunks, isLoading, isLastPage, loadMore } = useTweets(
+    context,
+    {
+      hashtagIds,
+      limit: 50,
+      withMedia: true,
+      initial,
+    }
+  )
+
+  const [isLoadingForLoadMore, setIsLoadingForLoadMore] = useState(false)
+
+  const handleLoadMore = async () => {
+    setIsLoadingForLoadMore(true)
+
+    try {
+      await loadMore()
+    } finally {
+      setIsLoadingForLoadMore(false)
+    }
+  }
+
+  return (
+    <>
+      {isLoading
+        ? (
+          <Text fontWeight="semibold" align="center" >
+            Loading...
+          </Text >
+        ) : (
+          <>
+            {EmbedTweets({ tweetsChunks })}
+            {!isLastPage && (
+              <Box m={4}>
+                <Button
+                  display="block"
+                  mx="auto"
+                  size="lg"
+                  variant="outline"
+                  colorScheme="twitter"
+                  isLoading={isLoadingForLoadMore}
+                  onClick={handleLoadMore}
+                >Load more!</Button>
+              </Box>
+            )}
+
+          </>
+        )}
+    </>
+  )
+}
+
+export default EmbedTweetsContainer
