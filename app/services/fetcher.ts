ecd36c260994020e2061a42b8f1f6f9a68aaaca5
komekami <komekami0131@gmail.com>
2023-04-10 07:52:58 +0900

    update: SWRにエラーハンドリングを追加

--- app/services/fetcher.ts
+++ app/services/fetcher.ts
@@ -1,3 +1,13 @@
 export const fetcher = (
   url: string
-) => fetch(url, { credentials: "include" }).then(r => r.json())
+) => fetch(url).then(res => {
+  // If the status code is not in the range 200-299,
+  // we still try to parse and throw it.
+  if (!res.ok) {
+    const error = new Error('An error occurred while fetching the data.')
+    // Attach extra info to the error object.
+    throw error
+  }
+
+  return res.json()
+})
